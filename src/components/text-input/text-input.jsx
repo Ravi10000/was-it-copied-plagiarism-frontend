0dd150edbe9aebf413ac5f4ac1e0058a35cdd6a0
ravi <pabhinav400@gmail.com>
2023-04-26 17:42:09 +0530

    fix show/hide toggle btn on password inputs

--- src/components/text-input/text-input.jsx
+++ src/components/text-input/text-input.jsx
@@ -13,7 +13,7 @@ export default function TextInput({ label, register, error, ...otherProps }) {
       <label className={styles.label} htmlFor={id}>
         {label}
       </label>
-      <input
+      {/* <input
         id={id}
         className={`${styles["text-input"]} ${
           otherProps?.type === "password" && styles.password
@@ -33,10 +33,39 @@ export default function TextInput({ label, register, error, ...otherProps }) {
           <img
             className={styles.toggleShow}
             src={`/eye-${show ? "opened" : "closed"}.png`}
+            // src={`/custom-icons/eye-${show ? "watching" : "closed"}.png`}
             alt=""
           />
         </div>
-      )}
+      )} */}
+      <div className={styles.innerInputContainer}>
+        <input
+          id={id}
+          className={`${styles["text-input"]} ${
+            otherProps?.type === "password" && styles.password
+          }`}
+          {...register}
+          {...otherProps}
+          type={
+            otherProps?.type === "password"
+              ? show
+                ? "text"
+                : "password"
+              : otherProps?.type
+          }
+        />
+        {otherProps?.type === "password" && (
+          // <div className={styles.togglePassword} onClick={handleTooglePassword}>
+          <img
+            onClick={handleTooglePassword}
+            className={styles.toggleShow}
+            src={`/eye-${show ? "opened" : "closed"}.png`}
+            // src={`/custom-icons/eye-${show ? "watching" : "closed"}.png`}
+            alt=""
+          />
+          // </div>
+        )}
+      </div>
       <label htmlFor={id} className={styles.errMsg}>
         {error}
       </label>
